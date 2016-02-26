dbf7a0fa9d7ce2e7fdd10108c5c3c560dd860144
Jeremy <jmanthey1@css.edu>
2016-02-25 21:17:21 -0600

    Finished Assignment - Jeremy Manthey

--- app/build/generated/source/r/debug/cis3334/simpletext/R.java
+++ app/build/generated/source/r/debug/cis3334/simpletext/R.java
@@ -42,23 +42,23 @@ public final class R {
         public static final int layout_second=0x7f060001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_second=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int title_activity_second=0x7f050003;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f040000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f040001;
     }
 }
--- app/src/main/java/cis3334/simpletext/SecondActivity.java
+++ app/src/main/java/cis3334/simpletext/SecondActivity.java
@@ -1,20 +1,18 @@
 package cis3334.simpletext;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
-import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.GregorianCalendar;
+
 public class SecondActivity extends Activity {
-	
+
 	String eventNum;		// number of the event from mainActivity
 	TextView tvTitle, tvSum, tvLoc, tvDate;
 
@@ -22,37 +20,36 @@ public class SecondActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_second);
-		
+
 		tvTitle = (TextView) findViewById(R.id.txtTitle);
 		tvSum = (TextView) findViewById(R.id.txtSummary);
 		tvLoc = (TextView) findViewById(R.id.txtLocation);
 		tvDate = (TextView) findViewById(R.id.txtDate);
 
-        // get the data passed from the main activity using extras
+		// get the data passed from the main activity using extras
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-            // grab the event number as a string from the main activity using extras
-		    eventNum = extras.getString("eventNum");
+			// grab the event number as a string from the main activity using extras
+			eventNum = extras.getString("eventNum");
 		}
 		//depending on the event number, display different information on this form
 		if (eventNum.equals("1")) {
-			tvTitle.setText("Katie McMahon’s");
-			tvSum.setText("Katie McMahon returns for a celebration of St. Patrick’s Day.  Originally from Dublin, and classically trained in voice and harp, she was a soloist with the original Riverdance troupe. She is joined by a Celtic band and dancers for this evening of everything Irish.");
+			tvTitle.setText("Twin Ports Choral Project: Spirit Moving Over Chaos");
+			tvSum.setText("Join the Twin Ports Choral Project as they explore music choral composed in remembrance of people and places torn apart by poverty, disaster, and war.");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText("FRIDAY, MARCH 13, 2015");
+			tvDate.setText("FRIDAY, FEBRUARY 26, 2016");
 		}
 		if (eventNum.equals("2")) {
-			tvTitle.setText("Randall Poole");
-			tvSum.setText("Randall A. Poole, Ph.D. is Professor of History at the College of St. Scholastica.  His research areas include Russian and European intellectual history, the history of ideas and the history of philosophical and religious thought.");
+			tvTitle.setText("An Evening with The Voice");
+			tvSum.setText("NBC’s The Voice’s Madi Davis and twins Andi & Alex Peot will perform in concert at Mitchell Auditorium in Duluth, MN on Sunday");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText("THURSDAY, MARCH 26, 2015");
+			tvDate.setText("Sunday, February 28, 2016");
 		}
 		if (eventNum.equals("3")) {
-			tvTitle.setText("Click Clack Moo!");
-			tvSum.setText("Cows that type? Hens on strike! Whoever heard of such a thing Farmer Brown cries When his granddaughter Jenny comes for a visit, Farmer Brown declared the farm a \"tech-free zone.\" He confiscates her laptop in the cold barn along with the shivering cows who use her computer to type messages requesting blankets.\n" +
-                    "\"No way,\" replies Farmer Brown. \"No blankets!\"");
+			tvTitle.setText("Thistles & Shamrocks");
+			tvSum.setText("An Evening of Scottish & Irish Music and Dance sponsored by the Duluth Scottish Heritage Association");
 			tvLoc.setText("Mitchell Auditorium");
-			tvDate.setText(" THURSDAY, FEBRUARY 26, 2015");
+			tvDate.setText("Friday, March 7, 2016");
 		}
 
 	}
@@ -63,17 +60,17 @@ public class SecondActivity extends Activity {
 		getMenuInflater().inflate(R.menu.layout_second, menu);
 		return true;
 	}
-	
+
 	public void onClickSendText (View v) {
-		
+
 		if (eventNum.equals("1")) {
-		
+
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Katie McMahon’s St. Patrick’s Day Party");
+			calIntent.putExtra(Events.TITLE, "Twin Ports Choral Project: Spirit Moving Over Chaos");
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
-			GregorianCalendar begDate = new GregorianCalendar(2015, 3, 13, 7, 30);
-			GregorianCalendar endDate = new GregorianCalendar(2015, 3, 13, 9, 30);
+			GregorianCalendar begDate = new GregorianCalendar(2016, 1, 26, 19, 30);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 1, 26, 21, 30);
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
 					begDate.getTimeInMillis());
 			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
@@ -83,33 +80,33 @@ public class SecondActivity extends Activity {
 		if (eventNum.equals("2")) {
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Randall Poole");
+			calIntent.putExtra(Events.TITLE, "An Evening with The Voice");
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
-            GregorianCalendar begDate = new GregorianCalendar(2015, 3, 26, 7, 30);
-            GregorianCalendar endDate = new GregorianCalendar(2015, 3, 26, 9, 30);
-            calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                    begDate.getTimeInMillis());
-            calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-                    endDate.getTimeInMillis());
+			GregorianCalendar begDate = new GregorianCalendar(2016, 1, 28, 16, 00);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 1, 28, 18, 30);
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+					begDate.getTimeInMillis());
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+					endDate.getTimeInMillis());
 			startActivity(calIntent);
 		}
 		if (eventNum.equals("3")) {
 			Intent calIntent = new Intent(Intent.ACTION_INSERT);
 			calIntent.setType("vnd.android.cursor.item/event");
-			calIntent.putExtra(Events.TITLE, "Click Clack Moo!d");
+			calIntent.putExtra(Events.TITLE, "Thistles & Shamrocks");
 			calIntent.putExtra(Events.EVENT_LOCATION, "Mitchell Auditorium");
-            GregorianCalendar begDate = new GregorianCalendar(2015, 2, 26, 12, 30);
-            GregorianCalendar endDate = new GregorianCalendar(2015, 2, 26, 2, 30);
-            calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                    begDate.getTimeInMillis());
-            calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-                    endDate.getTimeInMillis());
+			GregorianCalendar begDate = new GregorianCalendar(2016, 2, 4, 19, 00);
+			GregorianCalendar endDate = new GregorianCalendar(2016, 2, 4, 21, 30);
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+					begDate.getTimeInMillis());
+			calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+					endDate.getTimeInMillis());
 			startActivity(calIntent);
 
 		}
-		
+
 
 	}
-	
+
 
 }
