d29b12ec33d37f6fea90ccf70bb53c3f1fb9bd52
Jeremy <jmanthey1@css.edu>
2016-02-25 21:28:44 -0600

    Added images

--- app/build/generated/source/r/debug/cis3334/simpletext/R.java
+++ app/build/generated/source/r/debug/cis3334/simpletext/R.java
@@ -13,11 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int battlefieldband=0x7f020000;
         public static final int billymcmaughlin=0x7f020001;
-        public static final int clickclack=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int katie=0x7f020004;
-        public static final int randall=0x7f020005;
-        public static final int redbaraat=0x7f020006;
+        public static final int choral=0x7f020002;
+        public static final int clickclack=0x7f020003;
+        public static final int evening=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int katie=0x7f020006;
+        public static final int randall=0x7f020007;
+        public static final int redbaraat=0x7f020008;
+        public static final int thistles=0x7f020009;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
